7e5304bd18d432ae60e3c29be27f318cfab25a44
Dkyot <enclaverobobrain@gmail.com>
2023-03-09 01:20:33 +0300

    Added health system

---
+++ Assets/Scripts/Health System/Scripts/HeartHeal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class HeartHeal : MonoBehaviour
+{
+    [SerializeField] private int healAmount;
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        Player player = collider.GetComponent<Player>();
+        if (player != null) {
+            //player.Heal(healAmount);
+            Destroy(gameObject);
+        }
+    }
+
+}
