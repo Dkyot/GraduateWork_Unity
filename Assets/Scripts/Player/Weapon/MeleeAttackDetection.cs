e01ed496f148b58bdb7617f919eac71911d955d2
Dkyot <enclaverobobrain@gmail.com>
2023-03-20 14:19:16 +0300

    Added melee attack detection

---
+++ Assets/Scripts/Player/Weapon/MeleeAttackDetection.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MeleeAttackDetection : MonoBehaviour
+{
+    [SerializeField]
+    private Transform circleOrigin;
+    [SerializeField]
+    private float radius;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        Vector3 position = circleOrigin.position;
+        Gizmos.DrawWireSphere(position, radius);
+    }
+
+    public void DetectColliders() {
+        foreach(Collider2D collider in Physics2D.OverlapCircleAll(circleOrigin.position, radius)) {
+            Debug.Log(collider.name);
+        }
+    }
+}
