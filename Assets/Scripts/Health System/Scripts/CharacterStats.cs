03d18fb8303d073460e6cd52ff7fdfd310beb66f
Dkyot <enclaverobobrain@gmail.com>
2023-05-03 18:15:30 +0300

    Added health regeneration

--- Assets/Scripts/Health System/Scripts/CharacterStats.cs
+++ Assets/Scripts/Health System/Scripts/CharacterStats.cs
@@ -1,22 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    [SerializeField]
-    private int healthPoints = 5;
+    [SerializeField] private int healthPoints = 5;
 
     private HeartsHealthSystem heartsHealth;
 
-    void Awake() {
+    private void Awake() {
         heartsHealth = new HeartsHealthSystem(healthPoints);
-        //Debug.Log(heartsHealth.GetCurrentHP());
+    }
+
+    private void Start() {
+        //heartsHealth.SetHP(1);
+        //StartCoroutine(Regeneration(8, 1));
+        //StartCoroutine(PeriodicDamage(8, 1));
+    }
+    
+    private void Update() {
+        //Debug.Log(heartsHealth.GetCurrentHP());  
+    }
+
+    IEnumerator Regeneration(int hp, float timeInterval) {
+        for (int i = 0; i < hp; i++) {
+            heartsHealth.Heal(1);
+            yield return new WaitForSeconds(timeInterval);
+        }
+    }
+
+    IEnumerator PeriodicDamage(int hp, float timeInterval) {
+        for (int i = 0; i < hp; i++) {
+            heartsHealth.Damage(1);
+            yield return new WaitForSeconds(timeInterval);
+        }
     }
 
     public HeartsHealthSystem GetHealthSystem() {
         return heartsHealth;
     }
-
-    // private void Update() {
-    //     Debug.Log(heartsHealth.GetCurrentHP());
-    // }
 }
