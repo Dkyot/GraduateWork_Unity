883ca422a4f45a2cc2d8acf6c506a075b38b573b
Dkyot <enclaverobobrain@gmail.com>
2023-05-09 19:07:47 +0300

    Added main menu and pause menu

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool paused = false;
+    public GameObject pauseMenu;
+    public GameObject fpsCounter;
+    public GameObject coinCounter;
+    public GameObject health;
+
+    public PlayerInput playerInput;
+
+    public GameObject mobileInput;
+
+    private void Start() {
+        Play();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (paused)
+                Play();
+            else
+                Stop();
+        }
+    }
+
+    public void Play() {
+        pauseMenu.SetActive(false);
+        fpsCounter.SetActive(true);
+        coinCounter.SetActive(true);
+        health.SetActive(true);
+        playerInput.inputActions.InGamePlayerInput.Enable();
+
+            mobileInput.SetActive(Application.isMobilePlatform);
+
+        Time.timeScale = 1;
+        paused = false;
+    }
+
+    public void Stop() {
+        pauseMenu.SetActive(true);
+        fpsCounter.SetActive(false);
+        coinCounter.SetActive(false);
+        health.SetActive(false);
+        playerInput.inputActions.InGamePlayerInput.Disable();
+
+            mobileInput.SetActive(false);
+
+        Time.timeScale = 0;
+        paused = true;
+    }
+
+    public void ToMainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
